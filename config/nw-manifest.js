076f4ced06a6b112d7534e31b59c0f92ec4c6420
brambles <qjnight@gmail.com>
2017-06-13 16:46:03 +0800

    add tree node

--- config/nw-manifest.js
+++ config/nw-manifest.js
@@ -1,5 +1,13 @@
 'use strict'
 const package_json = require('../package.json')
-module.exports = Object.assign({}, package_json, {
+// module.exports = Object.assign({}, package_json, {
+//   main: 'index.html',
+// })
+
+module.exports = {
   main: 'index.html',
-})
+  name: package_json.name,
+  version: package_json.version,
+  author: package_json.author,
+  dependencies: package_json.dependencies
+}
---
+++ src/components/tree/Node.vue
@@ -0,0 +1,15 @@
+<template>
+
+  <li>{{node.name}}
+    <ul v-if="node.children && node.children.length">
+      <node v-for="child in node.children" :node="child" :key="child.name"></node>
+    </ul>
+  </li>
+</template>
+
+<script>
+export default {
+  name: 'node',
+  props: ['node'],
+}
+</script>
