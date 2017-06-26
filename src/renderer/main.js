8fd0c6ace56eeab04c097a6d30c09dd764dd9f4c
brambles <qjnight@gmail.com>
2017-06-26 19:21:19 +0800

    remove element-ui

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -5,15 +5,15 @@ import App from './App'
 import router from './router'
 import store from './store'
 
-import ElementUI from 'element-ui'
-import 'element-ui/lib/theme-default/index.css'
+// import ElementUI from 'element-ui'
+// import 'element-ui/lib/theme-default/index.css'
 
 if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
 Vue.http = Vue.prototype.$http = axios
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
-Vue.use(ElementUI)
+// Vue.use(ElementUI)
 
 new Vue({
   el: '#app',
